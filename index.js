032713647983d27bfcf8f0ca4f59725034eed2a1
Hanif Ahmad Syauqi <52845610+MRHRTZ@users.noreply.github.com>
2021-10-19 14:23:52 +0700

    Adding function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { musicallydown, ssstik, keeptiktok } = require('./src/function')
+const { musicallydown, ssstik, keeptiktok, tiktokdownload } = require('./src/function')
 
 exports.musicallydown = musicallydown
 exports.ssstik = ssstik
 exports.keeptiktok = keeptiktok
+exports.tiktokdownload = tiktokdownload
