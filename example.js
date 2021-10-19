80a3c895c00b146cf3cb7ce8655f4210a3915752
Hanif Ahmad Syauqi <52845610+MRHRTZ@users.noreply.github.com>
2021-10-19 14:24:48 +0700

    Updating new function

--- example.js
+++ example.js
@@ -1,12 +1,18 @@
 const tiktok = require('.')
 const url = 'https://www.tiktok.com/@youneszarou/video/6942436555692805381'
 
-
-tiktok.ssstik(url)
+tiktok.tiktokdownload(url)
      .then(result => {
           console.log(result)
      })
      .catch(e => console.log(e))
+
+
+// tiktok.ssstik(url)
+//      .then(result => {
+//           console.log(result)
+//      })
+//      .catch(e => console.log(e))
 
 
 // tiktok.musicallydown(url)
