135cbf53b41554789957eca3bcac0990ba885bea
Hanif Ahmad Syauqi <52845610+MRHRTZ@users.noreply.github.com>
2021-10-19 14:23:17 +0700

    Add new scrape

--- src/function/index.js
+++ src/function/index.js
@@ -2,6 +2,7 @@ const { default: Axios } = require('axios')
 const cheerio = require('cheerio')
 const qs = require('qs')
 const FormData = require('form-data')
+const qs = require('querystring')
 
 function ssstik(url) {
      return new Promise((resolve, reject) => {
@@ -180,6 +181,48 @@ function keeptiktok(url) {
      })
 }
 
+function tiktokdownload(url) {
+    return new Promise((resolve, reject) => {
+        Axios.get('https://ttdownloader.com/')
+        .then((data) => {
+            const $ = cheerio.load(data.data)
+            const cookie = data.headers['set-cookie'].join('')
+            const dataPost = {
+                url: url,
+                format: '', 
+                token: $('#token').attr('value')
+            }
+            // return console.log(cookie);
+            Axios({
+                method: 'POST',
+                url: 'https://ttdownloader.com/req/',
+                headers: {
+                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                    origin: 'https://ttdownloader.com',
+                    referer: 'https://ttdownloader.com/',
+                    cookie: cookie,
+                },
+                data: qs.stringify(dataPost)
+            }).then(({ data }) => {
+                const $ = cheerio.load(data)
+                const result = {
+                    nowm: $('#results-list > div:nth-child(2) > div.download > a')?.attr('href'),
+                    wm: $('#results-list > div:nth-child(3) > div.download > a')?.attr('href'),
+                    audio: $('#results-list > div:nth-child(4) > div.download > a').attr('href')
+                }
+                resolve(result);
+            })
+            .catch(e => {
+                reject({ status: false, message: 'error fetch data', e: e.message })
+            })
+        })
+        .catch(e => {
+            reject({ status: false, message: 'error fetch data', e: e.message })
+        })
+    })
+}
+
 module.exports.keeptiktok = keeptiktok
 module.exports.musicallydown = musicallydown
 module.exports.ssstik = ssstik
+module.exports.tiktokdownload = tiktokdownload
